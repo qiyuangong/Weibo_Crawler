44572665a1d771df52a301777df868483bbe81a1
darkfreedom <qiyuangong@gmail.com>
2014-04-03 21:22:28 +0800

    Add key.py and sina_weibo.db to example dir
    If you want this program run correctly, please modify key.py before
    running. Then move key.py and sina_weibo.db to root dir of this program.

---
+++ example/key.py
@@ -0,0 +1,9 @@
+#global variables for sina api 
+gl_USERID = ''						#your sina weibo username
+gl_PASSWD = ''						#your sina weibo password
+gl_APP_KEY = ['1218334239'] # app key
+gl_APP_SECRET = ['63b17f1ca7f4e2e1464d0ccd3324e668'] # app secret
+gl_ACCESS_TOKEN = []
+gl_EXPIRE_IN = []
+gl_CALLBACK_URL = 'https://api.weibo.com/oauth2/default.html' # callback url
+
